3d7e948eb1a9d8dc266debe4996456576e4d1068
RadimYassin <radimyassin20@gamil.com>
2023-04-08 22:57:30 +0200

    create pages and add style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/home'
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      hello
+       <BrowserRouter>
+       <Navbar/>
+       <div className='pages'>
+               <Routes>
+
+
+                <Route path='/' element={<Home/>} />
+               </Routes>
+       </div>
+       
+       
+       
+       </BrowserRouter>
+    
     </div>
   );
 }

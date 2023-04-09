311c53ce14b3f336912d483df49780251307b7d6
RadimYassin <radimyassin20@gamil.com>
2023-04-09 04:41:12 +0200

    updating

--- back end/server.js
+++ back end/server.js
@@ -4,9 +4,26 @@ require("dotenv").config();
 const express=require("express")
 const app=express()
 const Routes=require("./routes/workouts")
+const cors = require('cors');
+
 // mongoos
 const mongoos =require("mongoose")
 // middlware
+
+
+// middlware for link (backend to front end )
+const allowedOrigins = [process.env.PORTFRONT];
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json())
 app.use((req,res,next)=>{
 
